7663bf5c82bad19ff3777aa6a6824ac0d047af47
Chad <chadderbox@live.com>
2025-03-22 14:48:29 -0500

    Fix images attempt 1

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV == "production"
+
 const nextConfig: NextConfig = {
-  basePath: "/Portfolio",
+  basePath: isProd ? "/Portfolio" : "",
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
