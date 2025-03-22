8f3b3c88ba78b3f28f58632a295ef9edcf3bed9c
Chad <chadderbox@live.com>
2025-03-22 17:06:54 -0500

    Changed header background color. Finsished basic about me section. Finished basic skills section.

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,41 @@ import styles from "./page.module.css";
 export default function Home() {
   const content = (
     <main className={styles.main}>
+      
+      <hr/>
+      
       <div className={styles.about_me_cont}>
         <div className={styles.about_me_title}>About Me</div>
         <div className={styles.about_me}>
-          <p>With nearly 12 years of experience as a hobbyist programmer, I discovered a passion for data analytics through my studies. Since entering the workforce at age 14, I have utilized spreadsheets to manage finances and organize personal projects. Now, after completing multiple portfolio projects and the Google DA Certificate, I&#39;m equipped with professional knowledge and eager to transition into the field as a Data Analyst.</p>
+          <p>Hello! I'm <span className={styles.text_em}>Chad Jones</span>. With nearly <span className={styles.text_em}>12 years of experience as a hobbyist programmer</span>, I discovered a passion for data analytics through my studies. Since entering the workforce at age 14, I have utilized spreadsheets to manage finances and organize personal projects. Now, after completing <span className={styles.text_em}>multiple portfolio projects</span> and the <span className={styles.text_em}>Google DA Certificate</span>, I&#39;m equipped with professional knowledge and eager to transition into the field as a Data Analyst.</p>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div className={styles.skills_cont}>
+        <div className={styles.tech_skills_cont}>
+          <div className={styles.tech_skills_title}>Technical Skills</div>
+          <ul className={styles.tech_skills_list}>
+            <li>Spreadsheets (Excel / Google Sheets)</li>
+            <li>SQL</li>
+            <li>Python / R</li>
+            <li>Markdown / RMarkdown</li>
+            <li>Tableau</li>
+            <li>Git / Github</li>
+            <li>HTML / CSS / JavaScript</li>
+          </ul>
+        </div>
+
+        <div className={styles.soft_skills_cont}>
+        <div className={styles.soft_skills_title}>Soft Skills</div>
+          <ul className={styles.soft_skills_list}>
+            <li>Technical mindset driven to find automation and efficiency</li>
+            <li>Passion for overcoming challenges, solving complex problems, and finding patterns</li>
+            <li>Detail-oriented and organized</li>
+            <li>Works well both independently and while collaborating with a team</li>
+            <li>Excellent time management skills</li>
+          </ul>
         </div>
       </div>
     </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -17,39 +17,44 @@ export default function Header(){
                 </div>
 
                 <div className={styles.text_cont}>
-                    <div className="name">Chad Jones</div>
-                    <div className="title">Data Analyst</div>
+                    <div className={styles.name}>Chad Jones</div>
+                    <div className={styles.title}>Data Analyst</div>
                 </div>                
             </div>
 
             <nav className={styles.nav}>
                 <ul className={styles.list}>
-                    <li className={styles.list_item}><a href="#">Home</a></li>
-                    <li className={styles.list_item}><a href="#">Projects</a></li>
+                    <a className={styles.nav_link} href="/">
+                        <li>Home</li>
+                    </a>
+                    
+                    <a className={styles.nav_link} href="#">
+                        <li>Projects</li>
+                    </a>
 
                 </ul>   
             </nav>
 
             <div className={styles.contact_cont}>
-                <a href="clownchad@gmail.com">
+                <a className={styles.icon_link} href="clownchad@gmail.com">
                     <FaEnvelope 
                         className={styles.icon}
                         size={'2rem'}
                     />
                 </a>
-                <a href="https://www.linkedin.com/in/chad-j-524825348/">
+                <a className={styles.icon_link} href="https://www.linkedin.com/in/chad-j-524825348/">
                     <FaLinkedin 
                         className={styles.icon}
                         size={'2rem'}
                     />
                 </a>
-                <a href="https://github.com/ClownChad">
+                <a className={styles.icon_link} href="https://github.com/ClownChad">
                     <FaGithub 
                         className={styles.icon}
                         size={'2rem'}
                     />
                 </a>
-                <a href="https://www.kaggle.com/clownchad/code">
+                <a className={styles.icon_link} href="https://www.kaggle.com/clownchad/code">
                     <FaKaggle 
                         className={styles.icon_kaggle}
                         size={'2rem'}
